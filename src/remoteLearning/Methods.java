df944a82e05111f132177069c3525f6e92173ce7
palaszwaldemar <palasz.waldemar0@gmail.com>
2022-05-04 15:10:19 +0200

    Dodanie bibliotek

--- src/remoteLearning/Methods.java
+++ src/remoteLearning/Methods.java
@@ -1,6 +1,17 @@
 package remoteLearning;
 
-import javax.sound.midi.*;
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiEvent;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Sequencer;
+import javax.sound.midi.Sequence;
+import javax.sound.midi.Transmitter;
+import javax.sound.midi.Receiver;
+import javax.sound.midi.Track;
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
